11fdaac697857b426211f00f6bef0bdd8abafd55
Moody Salem <moody.salem@gmail.com>
2020-06-11 15:28:09 -0400

    more version update tests

--- src/isVersionUpdate.ts
+++ src/isVersionUpdate.ts
@@ -3,16 +3,16 @@ import { Version } from './types';
 /**
  * Returns true if versionB is an update over versionA
  */
-export function isVersionUpdate(versionA: Version, versionB: Version): boolean {
-  if (versionB.major > versionA.major) {
+export function isVersionUpdate(base: Version, update: Version): boolean {
+  if (update.major > base.major) {
     return true;
   }
-  if (versionB.major < versionA.major) {
+  if (update.major < base.major) {
     return false;
   }
-  if (versionB.minor > versionA.minor) {
+  if (update.minor > base.minor) {
     return true;
   }
-  if (versionB.minor < versionA.minor) return false;
-  return versionB.patch > versionA.patch;
+  if (update.minor < base.minor) return false;
+  return update.patch > base.patch;
 }
--- test/isVersionUpdate.test.ts
+++ test/isVersionUpdate.test.ts
@@ -1,7 +1,7 @@
 import { isVersionUpdate } from '../src';
 
 describe('#isVersionUpdate', () => {
-  it('major version increase', () => {
+  it('major version', () => {
     expect(
       isVersionUpdate(
         { major: 1, minor: 0, patch: 0 },
@@ -26,9 +26,21 @@ describe('#isVersionUpdate', () => {
         { major: 2, minor: 0, patch: 0 }
       )
     ).toEqual(true);
+    expect(
+      isVersionUpdate(
+        { major: 1, minor: 0, patch: 2 },
+        { major: 1, minor: 0, patch: 1 }
+      )
+    ).toEqual(false);
+    expect(
+      isVersionUpdate(
+        { major: 1, minor: 0, patch: 2 },
+        { major: 1, minor: 0, patch: 2 }
+      )
+    ).toEqual(false);
   });
 
-  it('minor version increase', () => {
+  it('minor version', () => {
     expect(
       isVersionUpdate(
         { major: 1, minor: 0, patch: 0 },
@@ -80,5 +92,17 @@ describe('#isVersionUpdate', () => {
         { major: 1, minor: 2, patch: 0 }
       )
     ).toEqual(true);
+    expect(
+      isVersionUpdate(
+        { major: 1, minor: 1, patch: 1 },
+        { major: 2, minor: 1, patch: 1 }
+      )
+    ).toEqual(true);
+    expect(
+      isVersionUpdate(
+        { major: 2, minor: 1, patch: 1 },
+        { major: 1, minor: 1, patch: 2 }
+      )
+    ).toEqual(false);
   });
 });
