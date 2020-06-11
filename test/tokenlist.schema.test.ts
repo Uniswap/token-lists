a0d7f5c66c1cfae13fee41404980a36f2768d67b
Moody Salem <moody.salem@gmail.com>
2020-06-11 14:52:04 -0400

    add more tests

--- test/tokenlist.schema.test.ts
+++ test/tokenlist.schema.test.ts
@@ -2,6 +2,11 @@ import Ajv from 'ajv';
 import { schema } from '../src';
 import exampleList from './testschemas/example.tokenlist.json';
 import emptyList from './testschemas/empty.tokenlist.json';
+import bigWords from './testschemas/bigwords.tokenlist.json';
+import invalidTokenAddress from './testschemas/invalidtokenaddress.tokenlist.json';
+import invalidVersion1 from './testschemas/invalidversion.1.tokenlist.json';
+import invalidVersion2 from './testschemas/invalidversion.2.tokenlist.json';
+import invalidVersion3 from './testschemas/invalidversion.3.tokenlist.json';
 
 const ajv = new Ajv({ allErrors: true });
 const validator = ajv.compile(schema);
@@ -10,53 +15,35 @@ describe('schema', () => {
   it('is valid', () => {
     expect(ajv.validateSchema(schema)).toEqual(true);
   });
+
+  function checkSchema(schema: any, valid: boolean): void {
+    expect(validator(schema)).toEqual(valid);
+    expect(validator.errors).toMatchSnapshot();
+  }
+
   it('requires name, timestamp, version, tokens', () => {
-    expect(validator({})).toEqual(false);
-    expect(validator.errors).toEqual([
-      {
-        keyword: 'required',
-        dataPath: '',
-        schemaPath: '#/required',
-        params: { missingProperty: 'name' },
-        message: "should have required property 'name'",
-      },
-      {
-        keyword: 'required',
-        dataPath: '',
-        schemaPath: '#/required',
-        params: { missingProperty: 'timestamp' },
-        message: "should have required property 'timestamp'",
-      },
-      {
-        keyword: 'required',
-        dataPath: '',
-        schemaPath: '#/required',
-        params: { missingProperty: 'version' },
-        message: "should have required property 'version'",
-      },
-      {
-        keyword: 'required',
-        dataPath: '',
-        schemaPath: '#/required',
-        params: { missingProperty: 'tokens' },
-        message: "should have required property 'tokens'",
-      },
-    ]);
+    checkSchema({}, false);
   });
+
   it('works for example schema', () => {
-    expect(validator(exampleList)).toEqual(true);
-    expect(validator.errors).toBeNull();
+    checkSchema(exampleList, true);
   });
+
   it('empty list fails', () => {
-    expect(validator(emptyList)).toEqual(false);
-    expect(validator.errors).toEqual([
-      {
-        dataPath: '.tokens',
-        keyword: 'minItems',
-        message: 'should NOT have fewer than 1 items',
-        params: { limit: 1 },
-        schemaPath: '#/properties/tokens/minItems',
-      },
-    ]);
+    checkSchema(emptyList, false);
+  });
+
+  it('fails with big names', () => {
+    checkSchema(bigWords, false);
+  });
+
+  it('checks token address', () => {
+    checkSchema(invalidTokenAddress, false);
+  });
+
+  it('checks version', () => {
+    checkSchema(invalidVersion1, false);
+    checkSchema(invalidVersion2, false);
+    checkSchema(invalidVersion3, false);
   });
 });
