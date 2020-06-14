0b3b6e0f19c15ebc1aed5c6eae2e7a21a69b9f45
Moody Salem <moody.salem@gmail.com>
2020-06-14 00:19:04 -0400

    test fix

--- test/types.test.ts
+++ test/types.test.ts
@@ -4,7 +4,7 @@ import exampleList from './schema/example.tokenlist.json';
 describe('types', () => {
   it('matches example schema', () => {
     // this is enough--typescript won't cast it unless it matches the interface
-    const list: TokenList = exampleList as TokenList;
+    const list: TokenList = exampleList;
 
     expect(list.name).toEqual('My Token List');
   });
